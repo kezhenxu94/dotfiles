f9702c94e1da54f603ac5ee83f6a411bb11b4ebf
kezhenxu94 <kezhenxu94@apache.org>
2024-11-20 19:40:20 +0800

    zsh: remove git branch from prompt as it is added in tmux status bar

--- zsh/configs/prompt.zsh
+++ zsh/configs/prompt.zsh
@@ -4,13 +4,6 @@
 # %j = number of background jobs
 BACKGROUND_JOBS='%B%(1j.%F{red}[%j] %f.)%b'
 
-git_prompt_info() {
-  current_branch=$(git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/ \1/')
-  if [[ -n $current_branch ]]; then
-    echo "$current_branch"
-  fi
-}
-
 theme() {
   echo "${$(dark-notify -e 2>/dev/null):-$THEME}"
 }
@@ -29,11 +22,6 @@ prompt_precmd() {
   PS1=''
   PS1+="%B%K{$BG}%F{white} %~ %k%b%f" && BG=$((BG+$STEP))
   PS1+="%B%K{$BG}%F{white} %k%b%f" && BG=$((BG+$STEP))
-
-  if [[ -n $(git_prompt_info) ]]; then
-    PS1+="%B%K{$BG}%F{white} $(git_prompt_info) %k%f%b" && BG=$((BG+$STEP))
-    PS1+="%B%K{$BG}%F{white} %k%b%f" && BG=$((BG+$STEP))
-  fi
 
   if [[ -n $VIRTUAL_ENV ]]; then
     PS1+="%B%K{$BG}%F{white} ($(basename $VIRTUAL_ENV)) %k%f%b" && BG=$((BG+$STEP))
