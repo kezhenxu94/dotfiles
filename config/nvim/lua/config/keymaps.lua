6bc531c4aeb08cd2cc49d8eb3f0b564dc85de2d9
kezhenxu94 <kezhenxu94@apache.org>
2025-10-12 22:01:42 +0800

    feat(nvim): keymaps to move lines

--- config/nvim/lua/config/keymaps.lua
+++ config/nvim/lua/config/keymaps.lua
@@ -4,6 +4,13 @@ vim.keymap.set("n", "<M-=>", "<C-w>=", { silent = true, desc = "Equalize window 
 vim.keymap.set("n", "<M->>", "<C-w>>", { silent = true, desc = "Increase window width" })
 vim.keymap.set("n", "<M-_>", "<C-w>-", { silent = true, desc = "Decrease window height" })
 
+vim.keymap.set("n", "<M-j>", "<cmd>execute 'move .+' . v:count1<cr>==", { desc = "Move Down" })
+vim.keymap.set("n", "<M-k>", "<cmd>execute 'move .-' . (v:count1 + 1)<cr>==", { desc = "Move Up" })
+vim.keymap.set("i", "<M-j>", "<esc><cmd>m .+1<cr>==gi", { desc = "Move Down" })
+vim.keymap.set("i", "<M-k>", "<esc><cmd>m .-2<cr>==gi", { desc = "Move Up" })
+vim.keymap.set("v", "<M-j>", ":<C-u>execute \"'<,'>move '>+\" . v:count1<cr>gv=gv", { desc = "Move Down" })
+vim.keymap.set("v", "<M-k>", ":<C-u>execute \"'<,'>move '<-\" . (v:count1 + 1)<cr>gv=gv", { desc = "Move Up" })
+
 vim.keymap.set("n", "<leader>bd", "<cmd>bdelete<cr>", { silent = true, desc = "Buffer Delete" })
 vim.keymap.set("n", "<leader>qq", "<cmd>qa<cr>", { silent = true, desc = "Exit Neovim" })
 vim.keymap.set("n", "<leader>qr", "<cmd>restart<cr>", { silent = true, desc = "Restart Neovim" })
