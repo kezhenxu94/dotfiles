8afd680fb72ffd02a00717fbf5201f91c9ed84d4
kezhenxu94 <kezhenxu94@apache.org>
2025-10-13 18:18:30 +0800

    feat(nvim): remove noice

--- config/nvim/lua/config/ui.lua
+++ config/nvim/lua/config/ui.lua
@@ -2,7 +2,6 @@ vim.pack.add({
   { src = "https://github.com/christoomey/vim-tmux-navigator" },
   { src = "https://github.com/folke/which-key.nvim" },
   { src = "https://github.com/MunifTanjim/nui.nvim" },
-  { src = "https://github.com/folke/noice.nvim" },
   { src = "https://github.com/nvim-tree/nvim-web-devicons" },
 }, { confirm = false })
 
@@ -16,47 +15,4 @@ require("which-key").setup({
   show_help = false,
   show_keys = false,
   preset = "helix",
-})
-
-require("noice").setup({
-  notify = {
-    enabled = false,
-  },
-  lsp = {
-    override = {
-      ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-      ["vim.lsp.util.stylize_markdown"] = true,
-    },
-  },
-  routes = {
-    {
-      filter = {
-        event = "msg_show",
-        any = {
-          { find = "%d+L, %d+B" },
-          { find = "; after #%d+" },
-          { find = "; before #%d+" },
-          { find = "Choose type .* to import" }, -- jdtls organize imports
-        },
-      },
-      view = "mini",
-    },
-  },
-  presets = {
-    bottom_search = true,
-    command_palette = true,
-    long_message_to_split = true,
-  },
-  cmdline = {
-    view = "cmdline",
-    format = {
-      cmdline = { pattern = "^:", icon = "", lang = "vim" },
-    },
-  },
-  popupmenu = {
-    enabled = false,
-  },
-  views = {
-    mini = { win_options = { winblend = 0 } },
-  },
 })
