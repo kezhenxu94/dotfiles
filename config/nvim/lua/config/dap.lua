92d3f0d3af0ed8626b603b6a6a5f1ffc3100c091
kezhenxu94 <kezhenxu94@apache.org>
2025-10-18 22:07:57 +0800

    chore(nvim): cleanup

--- config/nvim/lua/config/dap.lua
+++ config/nvim/lua/config/dap.lua
@@ -10,15 +10,10 @@ vim.keymap.set("n", "<leader>du", function()
     winfixwidth = true,
   }, "bo split")
 end, { desc = "Debugger UI" })
-vim.keymap.set("n", "<leader>db", function()
-  require("dap").toggle_breakpoint()
-end, { desc = "Debugger Breakpoint" })
-vim.keymap.set("n", "<leader>dc", function()
-  require("dap").continue()
-end, { desc = "Debugger Continue" })
-vim.keymap.set("n", "<leader>dl", function()
-  require("dap").run_last()
-end, { desc = "Debugger Last" })
-vim.keymap.set("n", "<leader>dt", function()
-  require("dap").terminate()
-end, { desc = "Debugger Terminate" })
+
+-- stylua: ignore start
+vim.keymap.set("n", "<leader>db", function() require("dap").toggle_breakpoint() end, { desc = "Debugger Breakpoint" })
+vim.keymap.set("n", "<leader>dc", function() require("dap").continue() end, { desc = "Debugger Continue" })
+vim.keymap.set("n", "<leader>dl", function() require("dap").run_last() end, { desc = "Debugger Last" })
+vim.keymap.set("n", "<leader>dt", function() require("dap").terminate() end, { desc = "Debugger Terminate" })
+-- stylua: ignore end
