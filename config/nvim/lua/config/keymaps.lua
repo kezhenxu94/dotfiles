e4f5648e57855b0247c96cb160993cc71a959591
kezhenxu94 <kezhenxu94@apache.org>
2025-10-18 12:24:35 +0800

    feat(nvim): keymaps diagnotics

--- config/nvim/lua/config/keymaps.lua
+++ config/nvim/lua/config/keymaps.lua
@@ -52,3 +52,14 @@ map("n", "<leader>tbg", function()
     vim.o.background = "dark"
   end
 end, { desc = "Toggle Background" })
+
+-- Diagnostics navigation
+-- stylua: ignore start
+map("n", "[d", function() vim.diagnostic.jump({ count = -vim.v.count1 }) end, { desc = "Previous Diagnostic" })
+map("n", "]d", function() vim.diagnostic.jump({ count = vim.v.count1 }) end, { desc = "Next Diagnostic" })
+map("n", "[e", function() vim.diagnostic.jump({ count = -vim.v.count1, severity = { min = vim.diagnostic.severity.ERROR } }) end, { desc = "Previous Diagnostic (Error)" })
+map("n", "]e", function() vim.diagnostic.jump({ count = vim.v.count1, severity = { min = vim.diagnostic.severity.ERROR } }) end, { desc = "Next Diagnostic (Error)" })
+map("n", "<leader>de", vim.diagnostic.open_float, { desc = "Show Diagnostic" })
+map("n", "<leader>sd", function() vim.diagnostic.setloclist() end, { desc = "Show Diagnostics (Buffer)" })
+map("n", "<leader>sD", function() vim.diagnostic.setqflist() end, { desc = "Show Diagnostics (Workspace)" })
+-- stylua: ignore end
