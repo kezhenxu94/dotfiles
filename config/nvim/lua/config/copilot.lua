958a225a2f3cb5281ac5db4b6fae7904027f38c8
kezhenxu94 <kezhenxu94@apache.org>
2025-10-18 14:51:58 +0800

    feat(nvim): replace with github copilot plugin

--- config/nvim/lua/config/copilot.lua
+++ config/nvim/lua/config/copilot.lua
@@ -1,27 +1,3 @@
 vim.pack.add({
-  { src = "https://github.com/folke/sidekick.nvim" },
+  { src = "https://github.com/github/copilot.vim" },
 }, { confirm = false })
-
-require("sidekick").setup({
-  cli = { enabled = false },
-})
-
-vim.keymap.set("n", "<tab>", function()
-  if require("sidekick").nes_jump_or_apply() then
-    return ""
-  end
-  if vim.lsp.inline_completion.get() then
-    return ""
-  end
-  return "<tab>"
-end, { expr = true, desc = "Goto/Apply Next Edit Suggestion" })
-
-vim.keymap.set("i", "<tab>", function()
-  if require("sidekick").nes_jump_or_apply() then
-    return ""
-  end
-  if vim.lsp.inline_completion.get() then
-    return ""
-  end
-  return "<tab>"
-end, { expr = true, desc = "Goto/Apply Next Edit Suggestion" })
