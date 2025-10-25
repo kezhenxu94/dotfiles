4cf36d48360b663ec58395b89ca77f1b3c258fdd
kezhenxu94 <kezhenxu94@apache.org>
2025-10-25 20:29:38 +0800

    feat(vim): move more nvim to vim autocmd

--- config/nvim/lua/config/autocmds.lua
+++
@@ -1,8 +0,0 @@
-local augroup = vim.api.nvim_create_augroup("kezhenxu94_autocmds", { clear = true })
-
-vim.api.nvim_create_autocmd("TextYankPost", {
-  group = augroup,
-  callback = function()
-    (vim.hl or vim.highlight).on_yank()
-  end,
-})
