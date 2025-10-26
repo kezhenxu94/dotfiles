bc86f5b345fa350b0e3dd7086b803d05405d276f
kezhenxu94 <kezhenxu94@apache.org>
2025-10-26 20:15:43 +0800

    feat(vim/nvim): tmux/session

--- config/nvim/lua/config/editor.lua
+++ config/nvim/lua/config/editor.lua
@@ -3,12 +3,6 @@ vim.pack.add({
   { src = "https://github.com/folke/which-key.nvim" },
 }, { confirm = false })
 
-vim.keymap.set("n", "<C-\\>", "<cmd>TmuxNavigatePrevious<cr>", { desc = "Go to the previous pane" })
-vim.keymap.set("n", "<C-h>", "<cmd>TmuxNavigateLeft<cr>", { desc = "Got to the left pane" })
-vim.keymap.set("n", "<C-j>", "<cmd>TmuxNavigateDown<cr>", { desc = "Got to the down pane" })
-vim.keymap.set("n", "<C-k>", "<cmd>TmuxNavigateUp<cr>", { desc = "Got to the up pane" })
-vim.keymap.set("n", "<C-l>", "<cmd>TmuxNavigateRight<cr>", { desc = "Got to the right pane" })
-
 require("which-key").setup({
   show_help = false,
   show_keys = false,
---
+++ config/vim/vimrc.tmux
@@ -0,0 +1,6 @@
+" Tmux Navigator keymaps
+nnoremap <C-\> :TmuxNavigatePrevious<CR>
+nnoremap <C-h> :TmuxNavigateLeft<CR>
+nnoremap <C-j> :TmuxNavigateDown<CR>
+nnoremap <C-k> :TmuxNavigateUp<CR>
+nnoremap <C-l> :TmuxNavigateRight<CR>
