0ed8e28a26996efd11971e9d407ddad9402d676b
kezhenxu94 <kezhenxu94@apache.org>
2025-10-26 20:31:02 +0800

    feat(vim/nvim): quickfix

--- config/nvim/lua/config/quickfix.lua
+++ config/nvim/lua/config/quickfix.lua
@@ -166,13 +166,3 @@ vim.api.nvim_create_autocmd("BufWinLeave", {
     end
   end,
 })
-
-local map = vim.keymap.set
-map("n", "<leader>qq", function()
-  local is_open = vim.fn.getqflist({ winid = 1 }).winid ~= 0
-  if is_open then
-    vim.cmd("cclose")
-  else
-    vim.cmd("copen")
-  end
-end, { desc = "Open Quickfix" })
