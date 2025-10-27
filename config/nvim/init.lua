d4df117d7f994ddc429918c2d8c951e827cfc79a
kezhenxu94 <kezhenxu94@apache.org>
2025-10-27 09:24:53 +0800

    fix(nvim): move state folder set

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,3 +1,22 @@
+vim.loop = vim.uv or vim.loop
+
+local vim_info_dir = vim.fn.getcwd() .. "/.vim"
+vim.g.vim_info_dir = vim_info_dir
+
+if vim.loop.fs_stat(vim_info_dir) == nil and vim.loop.fs_access(vim.fn.getcwd(), "W") then
+  vim.fn.mkdir(vim_info_dir)
+end
+vim.opt.shadafile = vim_info_dir .. "/main.shada"
+
+for _, name in ipairs({ "state" }) do
+  local folder = vim_info_dir .. "/" .. name
+  if vim.loop.fs_stat(folder) == nil and vim.loop.fs_access(vim_info_dir, "W") then
+    vim.fn.mkdir(folder)
+  end
+
+  vim.env[("XDG_%s_HOME"):format(name:upper())] = folder
+end
+
 require("config.options")
 require("config.keymaps")
 require("config.autocmds")
--- config/nvim/lua/config/options.lua
+++ config/nvim/lua/config/options.lua
@@ -1,24 +1,5 @@
 vim.cmd("source $XDG_CONFIG_HOME/vim/vimrc")
 
-vim.loop = vim.uv or vim.loop
-
-local vim_info_dir = vim.fn.getcwd() .. "/.vim"
-vim.g.vim_info_dir = vim_info_dir
-
-if vim.loop.fs_stat(vim_info_dir) == nil and vim.loop.fs_access(vim.fn.getcwd(), "W") then
-  vim.fn.mkdir(vim_info_dir)
-end
-vim.opt.shadafile = vim_info_dir .. "/main.shada"
-
-for _, name in ipairs({ "state" }) do
-  local folder = vim_info_dir .. "/" .. name
-  if vim.loop.fs_stat(folder) == nil and vim.loop.fs_access(vim_info_dir, "W") then
-    vim.fn.mkdir(folder)
-  end
-
-  vim.env[("XDG_%s_HOME"):format(name:upper())] = folder
-end
-
 vim.filetype.add({
   pattern = {
     [".*/git/config"] = "gitconfig",
