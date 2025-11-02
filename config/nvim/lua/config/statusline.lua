f501b775e56c9aa98c83240b3c620b6b31dddead
kezhenxu94 <kezhenxu94@apache.org>
2025-11-02 20:52:44 +0800

    feat(nvim): status line

---
+++ config/nvim/lua/config/statusline.lua
@@ -0,0 +1,43 @@
+_G.lsp_progress = ""
+
+vim.api.nvim_create_autocmd("LspProgress", {
+  callback = function(ev)
+    local client = vim.lsp.get_client_by_id(ev.data.client_id)
+    local name = client and client.name or "LSP"
+
+    local value = ev.data.params.value
+    local parts = {}
+    table.insert(parts, "[" .. name .. "]")
+
+    if value.kind == "end" then
+      table.insert(parts, "✔ Done")
+    else
+      if value.title and value.title ~= "" then
+        table.insert(parts, value.title)
+      end
+
+      if value.message and value.message ~= "" then
+        table.insert(parts, value.message)
+      end
+
+      if value.percentage then
+        table.insert(parts, string.format("(%d%%)", value.percentage))
+      end
+    end
+    _G.lsp_progress = table.concat(parts, " ")
+    if value.kind == "end" then
+      vim.defer_fn(function()
+        _G.lsp_progress = ""
+        vim.cmd("redrawstatus")
+      end, 5000)
+    end
+
+    vim.cmd("redrawstatus")
+  end,
+})
+
+function _G.get_lsp_progress()
+  return _G.lsp_progress
+end
+
+vim.opt.statusline = "%f %h%m%r%=%{v:lua.get_lsp_progress()}"
