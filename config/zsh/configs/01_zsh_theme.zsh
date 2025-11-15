ad7a9a40e0fd4a8505d055fcee0744b0a5e83bf6
kezhenxu94 <kezhenxu94@apache.org>
2025-11-15 17:16:37 +0800

    fix: theme

--- config/zsh/configs/01_zsh_theme.zsh
+++ config/zsh/configs/01_zsh_theme.zsh
@@ -1,25 +1,19 @@
 #!/usr/bin/env zsh
 
 function _set_zsh_theme() {
-  if [[ -z "$TMUX" ]]; then
-    return
-  fi
   if ! command -v dark-notify > /dev/null 2>&1; then
     if which tmux > /dev/null && tmux showenv -g THEME 2>&1 > /dev/null; then
       export THEME=$(tmux showenv -g THEME 2>/dev/null | cut -d= -f2)
     fi
     return
   fi
-  export THEME=light
+  export THEME=${THEME:-light}
   if [ "$(dark-notify -e 2>/dev/null)" = "dark" ]; then
     export THEME=dark
   fi
 }
 
 function _set_zsh_syntax_highlighting() {
-  if [[ -z "$TMUX" ]]; then
-    return
-  fi
   if [[ "$THEME" == "light" ]]; then
     source "$XDG_CONFIG_HOME"/zsh/plugins/cappuccin/themes/catppuccin_latte-zsh-syntax-highlighting.zsh
   else
