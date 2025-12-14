735fe75aec75bf0716c6c5ac3c2ef94cf7a5dc97
kezhenxu94 <kezhenxu94@apache.org>
2025-12-14 23:20:17 +0800

    chore(nvim): change to minifile

--- config/nvim/lua/config/minifiles.lua
+++ config/nvim/lua/config/minifiles.lua
@@ -15,3 +15,21 @@ vim.keymap.set("n", "<leader>E", function()
     minifiles.open(vim.fn.getcwd())
   end
 end, { desc = "Toggle mini.files (cwd)" })
+
+-- Add keymap for window picker integration
+local winpick = require("config.winpick")
+vim.api.nvim_create_autocmd("User", {
+  pattern = "MiniFilesBufferCreate",
+  callback = function(args)
+    local buf_id = args.data.buf_id
+    vim.keymap.set("n", "<c-o>", function()
+      local entry = minifiles.get_fs_entry()
+      if not entry or entry.fs_type ~= "file" then
+        vim.notify("Not a file", vim.log.levels.WARN)
+        return
+      end
+      minifiles.close()
+      winpick.open_in_window(entry.path, { exclude_current = false })
+    end, { buffer = buf_id, desc = "Open file in picked window" })
+  end,
+})
