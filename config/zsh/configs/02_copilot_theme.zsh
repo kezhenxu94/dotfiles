2f6d1d9b83be22e369a6fcc484930f9eba61f5e3
kezhenxu94 <kezhenxu94@apache.org>
2025-12-22 19:15:41 +0800

    Revert "refactor(copilot): remove copilot theme configuration"
    This reverts commit 409235f9488a9add437b834edde1e64186563901.

---
+++ config/zsh/configs/02_copilot_theme.zsh
@@ -0,0 +1,22 @@
+#!/usr/bin/env zsh
+
+SCRIPT_DIR=$(cd "$(dirname "$0")" && pwd)/../..
+CONFIG_FILE="$SCRIPT_DIR/.copilot/config.json"
+echo $CONFIG_FILE
+
+function _set_copilot_theme {
+  if [[ ! -f $CONFIG_FILE ]]; then
+    return
+  fi
+  if [[ "$THEME" == "light" ]]; then
+    tmpfile=$(mktemp)
+    jq '.theme = "light"' "$CONFIG_FILE" > "$tmpfile" && mv "$tmpfile" "$CONFIG_FILE" 
+  else
+    tmpfile=$(mktemp)
+    jq '.theme = "dark"' "$CONFIG_FILE"  > "$tmpfile" && mv "$tmpfile" "$CONFIG_FILE" 
+  fi
+}
+
+autoload -U add-zsh-hook
+add-zsh-hook precmd _set_copilot_theme
+_set_copilot_theme
