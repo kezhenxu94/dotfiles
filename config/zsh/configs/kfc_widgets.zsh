3f65f9105e5154bc058f37857319eabf33a70242
kezhenxu94 <kezhenxu94@apache.org>
2026-02-13 18:35:23 +0800

    chore: update kfc submodule path and clean up pager settings
    - Update kfc widget to source from new installation path ~/.bin/kfc
    - Remove redundant MANPAGER settings (relying on nvim default)
    - KFC_WIDGET=1 enables the widget when sourcing k script

--- config/zsh/configs/kfc_widgets.zsh
+++ config/zsh/configs/kfc_widgets.zsh
@@ -1,13 +1,2 @@
-[[ -z "$commands[kubectl]" ]] && return
-
-_kfc_widget() {
-  local result
-  result=$(k)
-  if [[ -n "$result" ]]; then
-    LBUFFER+="$result"
-  fi
-  zle reset-prompt
-}
-
-zle -N _kfc_widget
-bindkey -M viins '^k' _kfc_widget
+export PATH="$HOME/.bin/kfc:$PATH"
+KFC_WIDGET=1 source "$HOME/.bin/kfc/k"
