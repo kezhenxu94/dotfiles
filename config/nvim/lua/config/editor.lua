a3de57ec7f21a332e506b624bd9f5fe54e941e37
kezhenxu94 <kezhenxu94@apache.org>
2026-04-25 23:10:47 +0800

    chore(nvim): clean up

--- config/nvim/lua/config/editor.lua
+++ config/nvim/lua/config/editor.lua
@@ -1,7 +1,5 @@
 vim.pack.add({ "https://github.com/folke/which-key.nvim" }, { confirm = false })
-vim.pack.add({ "https://github.com/kezhenxu94/nvim-claude-lsp" }, { confirm = false })
 
-require("nvim-claude-lsp").setup()
 require("which-key").setup({
   show_help = false,
   show_keys = false,
