be7990341b4e43ea64afdab1648de87ab4b7a6f4
kezhenxu94 <kezhenxu94@apache.org>
2026-06-30 09:33:13 +0800

    feat(nvim): add treesitter textobject keymaps and repeatable moves
    Wire up select/move keymaps for function and class textobjects, plus
    ; and , to repeat the last textobject move.

--- config/nvim/lua/config/lsp/treesitter.lua
+++ config/nvim/lua/config/lsp/treesitter.lua
@@ -44,3 +44,30 @@ vim.api.nvim_create_autocmd("FileType", {
 
 vim.opt.foldmethod = "expr"
 vim.opt.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+
+-- keymaps
+local ts_repeat_move = require("nvim-treesitter-textobjects.repeatable_move")
+
+vim.keymap.set({ "x", "o" }, "am", function()
+  require("nvim-treesitter-textobjects.select").select_textobject("@function.outer", "textobjects")
+end)
+vim.keymap.set({ "x", "o" }, "im", function()
+  require("nvim-treesitter-textobjects.select").select_textobject("@function.inner", "textobjects")
+end)
+vim.keymap.set({ "x", "o" }, "ac", function()
+  require("nvim-treesitter-textobjects.select").select_textobject("@class.outer", "textobjects")
+end)
+vim.keymap.set({ "x", "o" }, "ic", function()
+  require("nvim-treesitter-textobjects.select").select_textobject("@class.inner", "textobjects")
+end)
+
+-- movements
+vim.keymap.set({ "n", "x", "o" }, "]c", function()
+  require("nvim-treesitter-textobjects.move").goto_next_start("@class.outer", "textobjects")
+end)
+vim.keymap.set({ "n", "x", "o" }, "[c", function()
+  require("nvim-treesitter-textobjects.move").goto_previous_start("@class.outer", "textobjects")
+end)
+
+vim.keymap.set({ "n", "x", "o" }, ";", ts_repeat_move.repeat_last_move_next)
+vim.keymap.set({ "n", "x", "o" }, ",", ts_repeat_move.repeat_last_move_previous)
