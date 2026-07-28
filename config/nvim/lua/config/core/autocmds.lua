c90b05bcd22765fe02f7112c574858f58564ce13
kezhenxu94 <kezhenxu94@apache.org>
2026-07-28 20:18:22 +0800

    fix(nvim): use hl_op for yank highlight autocmd

--- config/nvim/lua/config/core/autocmds.lua
+++ config/nvim/lua/config/core/autocmds.lua
@@ -57,6 +57,6 @@ vim.api.nvim_create_autocmd("FileType", {
 vim.api.nvim_create_autocmd("TextYankPost", {
   group = vim.api.nvim_create_augroup("highlight_on_yank", { clear = true }),
   callback = function()
-    (vim.hl or vim.highlight).on_yank()
+    (vim.hl or vim.highlight).hl_op()
   end,
 })
